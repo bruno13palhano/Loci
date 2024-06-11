417d7baf01e35db30b1b40ff503b67d941b7b533
bruno13palhano <bruno13palhano@gmail.com>
2024-06-11 11:12:19 -0300

    Rename property

--- model/src/main/java/com/bruno13palhano/model/ServiceData.kt
+++ model/src/main/java/com/bruno13palhano/model/ServiceData.kt
@@ -2,7 +2,7 @@ package com.bruno13palhano.model
 
 data class ServiceData(
     val id: Long,
-    val name: String,
+    val title: String,
     val description: String,
     val price: Float,
     val images: List<String>
