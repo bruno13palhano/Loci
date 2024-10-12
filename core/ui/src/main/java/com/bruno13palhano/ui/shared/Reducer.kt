0ef6bc85a011b5baf66006d54e46ffcbb33eae70
bruno13palhano <bruno13palhano@gmail.com>
2024-10-12 09:22:12 -0300

    Rename parameter from reduce method

--- core/ui/src/main/java/com/bruno13palhano/ui/shared/Reducer.kt
+++ core/ui/src/main/java/com/bruno13palhano/ui/shared/Reducer.kt
@@ -1,5 +1,5 @@
 package com.bruno13palhano.ui.shared
 
 interface Reducer<State: ViewState, Event: ViewEvent, Effect: ViewEffect> {
-    fun reduce(state: State, event: Event): Pair<State, Effect?>
+    fun reduce(previousState: State, event: Event): Pair<State, Effect?>
 }
