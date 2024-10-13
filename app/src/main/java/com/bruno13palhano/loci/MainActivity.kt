e697808b22d16b360292f194d045b02237680042
bruno13palhano <bruno13palhano@gmail.com>
2024-10-12 21:57:36 -0300

    Set app navigation

--- app/src/main/java/com/bruno13palhano/loci/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/loci/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.bruno13palhano.loci.ui.menu.BottomMenu
+import com.bruno13palhano.loci.ui.navigation.MainNavGraph
 import com.bruno13palhano.loci.ui.theme.LociTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,32 +23,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LociTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(
+                        bottomBar = {
+                            BottomMenu(navController = navController)
+                        }
+                    ) { innerPadding ->
+                        MainNavGraph(
+                            modifier = Modifier.padding(innerPadding),
+                            navController = navController
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LociTheme {
-        Greeting("Android")
     }
 }
