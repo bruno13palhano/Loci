b23d0f08aa1181182b366ac76e9b11b2c416d4cd
bruno13palhano <bruno13palhano@gmail.com>
2024-10-13 20:23:13 -0300

    Add login module dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(project(":feature:login"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
