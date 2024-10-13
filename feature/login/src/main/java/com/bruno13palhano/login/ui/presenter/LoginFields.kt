5c44d53f45f015c7e5b7f7a7139f7b86eda4b1e0
bruno13palhano <bruno13palhano@gmail.com>
2024-10-13 12:09:57 -0300

    Add a wrapper for login input fields

---
+++ feature/login/src/main/java/com/bruno13palhano/login/ui/presenter/LoginFields.kt
@@ -0,0 +1,21 @@
+package com.bruno13palhano.login.ui.presenter
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import javax.inject.Inject
+
+internal class LoginFields @Inject constructor() {
+    var email by mutableStateOf("")
+        private set
+    var password by mutableStateOf("")
+        private set
+
+    fun updateEmail(email: String) {
+        this.email = email
+    }
+
+    fun updatePassword(password: String) {
+        this.password = password
+    }
+}
