33f4b05e2a4669773dc8994f86c1181f2c3b1163
bruno13palhano <bruno13palhano@gmail.com>
2024-10-13 12:51:49 -0300

    Add a method to check for any invalid properties

--- feature/login/src/main/java/com/bruno13palhano/login/ui/presenter/LoginFields.kt
+++ feature/login/src/main/java/com/bruno13palhano/login/ui/presenter/LoginFields.kt
@@ -18,4 +18,6 @@ internal class LoginFields @Inject constructor() {
     fun updatePassword(password: String) {
         this.password = password
     }
+
+    fun isValid() = email.isNotBlank() && password.isNotBlank()
 }
