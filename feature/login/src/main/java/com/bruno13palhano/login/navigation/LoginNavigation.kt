2c43d6dc47622435a723f7e33483aba464604b7e
bruno13palhano <bruno13palhano@gmail.com>
2024-10-17 22:33:06 -0300

    Remove other routes and navController

--- feature/login/src/main/java/com/bruno13palhano/login/navigation/LoginNavigation.kt
+++ feature/login/src/main/java/com/bruno13palhano/login/navigation/LoginNavigation.kt
@@ -1,41 +1,30 @@
 package com.bruno13palhano.login.navigation
 
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
 import com.bruno13palhano.login.ui.login.presenter.LoginRoute
 import kotlinx.serialization.Serializable
 
 fun NavGraphBuilder.loginScreen(
     modifier: Modifier = Modifier,
-    navController: NavController,
+    navigateToHome: () -> Unit,
+    navigateToNewAccount: () -> Unit,
+    navigateToForgotPassword: () -> Unit,
     showBottomMenu: (show: Boolean) -> Unit
 ) {
-    navigation<LoginRoutes.MainLogin>(startDestination = LoginRoutes.Login) {
-        composable<LoginRoutes.Login> {
-            showBottomMenu(false)
-            LoginRoute(
-                modifier = modifier,
-                navigateToHome = {},
-                navigateToNewAccount = {},
-                navigateToForgotPassword = {}
-            )
-        }
+    composable<LoginRoutes.Login> {
+        showBottomMenu(false)
+        LoginRoute(
+            modifier = modifier,
+            navigateToHome = navigateToHome,
+            navigateToNewAccount = navigateToNewAccount,
+            navigateToForgotPassword = navigateToForgotPassword
+        )
     }
 }
 
 sealed interface LoginRoutes {
     @Serializable
-    data object MainLogin : LoginRoutes
-
-    @Serializable
     data object Login : LoginRoutes
-
-    @Serializable
-    data object CreateAccount : LoginRoutes
-
-    @Serializable
-    data object ForgotPassword : LoginRoutes
 }
