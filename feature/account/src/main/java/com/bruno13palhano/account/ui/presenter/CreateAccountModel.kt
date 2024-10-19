e22185d733bd076abb3bb1fe1db6da172e64dcd1
bruno13palhano <bruno13palhano@gmail.com>
2024-10-19 08:41:17 -0300

    Add CreateAccountModel

---
+++ feature/account/src/main/java/com/bruno13palhano/account/ui/presenter/CreateAccountModel.kt
@@ -0,0 +1,48 @@
+package com.bruno13palhano.account.ui.presenter
+
+import androidx.compose.runtime.Immutable
+import com.bruno13palhano.ui.shared.ViewAction
+import com.bruno13palhano.ui.shared.ViewEffect
+import com.bruno13palhano.ui.shared.ViewEvent
+import com.bruno13palhano.ui.shared.ViewState
+
+@Immutable
+internal data class CreateAccountState(
+    val loading: Boolean,
+    val createAccountFields: CreateAccountFields,
+    val error: Boolean,
+    val passwordVisible: Boolean,
+    val confirmPasswordVisible: Boolean
+) : ViewState {
+    companion object {
+        val Initial = CreateAccountState(
+            loading = false,
+            createAccountFields = CreateAccountFields(),
+            error = false,
+            passwordVisible = false,
+            confirmPasswordVisible = false
+        )
+    }
+}
+
+@Immutable
+internal sealed interface CreateAccountEvent : ViewEvent {
+    data object Done : CreateAccountEvent
+    data object Error : CreateAccountEvent
+    data object TogglePasswordVisibility : CreateAccountEvent
+    data object ToggleConfirmPasswordVisibility : CreateAccountEvent
+    data object NavigateToHome : CreateAccountEvent
+}
+
+@Immutable
+internal sealed interface CreateAccountEffect : ViewEffect {
+    data object ShowError : CreateAccountEffect
+    data object NavigateToHome : CreateAccountEffect
+}
+
+@Immutable
+internal sealed interface CreateAccountAction : ViewAction {
+    data object OnDone : CreateAccountAction
+    data object OnTogglePasswordVisibility : CreateAccountAction
+    data object OnToggleConfirmPasswordVisibility : CreateAccountAction
+}
