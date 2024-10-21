6759ef84b254f8ea1bb0dbc160bfd40fd1035188
bruno13palhano <bruno13palhano@gmail.com>
2024-10-21 09:47:22 -0300

    Doc Reducer interface

--- core/ui/src/main/java/com/bruno13palhano/ui/shared/Reducer.kt
+++ core/ui/src/main/java/com/bruno13palhano/ui/shared/Reducer.kt
@@ -1,5 +1,24 @@
 package com.bruno13palhano.ui.shared
 
+/**
+ * Interface for a reducer.
+ *
+ * @param State the state of the view.
+ *
+ * @param Event the event to change the state and trigger a side effect if needed.
+ *
+ * @param Effect the side effect that this event will trigger.
+ */
 interface Reducer<State: ViewState, Event: ViewEvent, Effect: ViewEffect> {
+    /**
+     * Reduce the state of the view based on the event.
+     *
+     * @param previousState the previous state of the view.
+     *
+     * @param event the event to change the state and trigger a side effect if necessary.
+     *
+     * @return a [Pair] of the new [State] and the side [Effect] that will be
+     * triggered if necessary.
+     */
     fun reduce(previousState: State, event: Event): Pair<State, Effect?>
 }
