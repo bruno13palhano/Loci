ab76ae10ed2a8e1843f6262f88575e09eac17e74
bruno13palhano <bruno13palhano@gmail.com>
2024-10-21 16:07:33 -0300

    Doc ActionProcessor and BaseViewModel

--- core/ui/src/main/java/com/bruno13palhano/ui/shared/ActionProcessor.kt
+++ core/ui/src/main/java/com/bruno13palhano/ui/shared/ActionProcessor.kt
@@ -1,5 +1,19 @@
 package com.bruno13palhano.ui.shared
 
+/**
+ * Interface for the action processor.
+ *
+ * @param Action The type of action to process.
+ *
+ * @param Event The type of event to return.
+ */
 interface ActionProcessor<Action: ViewAction, Event: ViewEvent> {
+    /**
+     * Processes the given action and returns an event.
+     *
+     * @param action The [Action] to process.
+     *
+     * @return The [Event] resulting from processing the action.
+     */
     fun process(action: Action): Event
 }
--- core/ui/src/main/java/com/bruno13palhano/ui/shared/BaseViewModel.kt
+++ core/ui/src/main/java/com/bruno13palhano/ui/shared/BaseViewModel.kt
@@ -13,13 +13,24 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 
+/**
+ * Base ViewModel to coordinate the states, events and side effect operations.
+ *
+ * @property actionProcessor The [ActionProcessor] to process the actions.
+ *
+ * @property reducer The [Reducer] to reduce the states.
+ *
+ * @property state The [StateFlow] containing the current state to be collected.
+ *
+ * @property effects The [Flow] with the side effects to be collected.
+ */
 abstract class BaseViewModel<State: ViewState, Event: ViewEvent, Effect: ViewEffect, Action: ViewAction>(
     protected val actionProcessor: ActionProcessor<Action, Event>,
     protected val reducer: Reducer<State, Event, Effect>
 ) : ViewModel() {
     private val scope = CoroutineScope(viewModelScope.coroutineContext + AndroidUiDispatcher.Main)
 
-    val events: MutableSharedFlow<Event> = MutableSharedFlow(extraBufferCapacity = 20)
+    private val events: MutableSharedFlow<Event> = MutableSharedFlow(extraBufferCapacity = 20)
 
     private val _effects = Channel<Effect>(capacity = Channel.CONFLATED)
     val effects = _effects.receiveAsFlow()
@@ -30,18 +41,34 @@ abstract class BaseViewModel<State: ViewState, Event: ViewEvent, Effect: ViewEff
         }
     }
 
+    /**
+     * Update the state based on the event in a reactive way.
+     *
+     * @param events The [Flow] containing the [Event] to be processed.
+     *
+     * @return The new [State].
+     */
     @Composable
     protected abstract fun states(events: Flow<Event>): State
 
+    /**
+     * Called by the View to process an action.
+     */
     fun onAction(action: Action) {
         sendEvent(event = actionProcessor.process(action))
     }
 
+    /**
+     * Called by the ViewModel to send an event.
+     */
     protected fun sendEvent(event: Event) {
         events.tryEmit(event)
     }
 
-    fun sendEffect(effect: Effect) {
+    /**
+     * Called by the ViewModel to send an effect.
+     */
+    protected fun sendEffect(effect: Effect) {
         _effects.trySend(effect)
     }
 }
