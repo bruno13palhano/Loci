3711aef67c0fa75ff11c560c4774149fe6c1a194
bruno13palhano <bruno13palhano@gmail.com>
2024-10-24 20:42:29 -0300

    Refactor CreateAccountPresenter

--- feature/account/src/main/java/com/bruno13palhano/account/ui/presenter/CreateAccountPresenter.kt
+++ feature/account/src/main/java/com/bruno13palhano/account/ui/presenter/CreateAccountPresenter.kt
@@ -2,7 +2,7 @@ package com.bruno13palhano.account.ui.presenter
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import com.bruno13palhano.ui.shared.Reducer
@@ -17,6 +17,18 @@ internal fun createAccountPresenter(
 ): CreateAccountState {
     val state = remember { mutableStateOf(CreateAccountState.Initial) }
 
+    HandleEvents(events = events, state = state, reducer = reducer, sendEffect = sendEffect)
+
+    return state.value
+}
+
+@Composable
+private fun HandleEvents(
+    events: Flow<CreateAccountEvent>,
+    state: MutableState<CreateAccountState>,
+    reducer: Reducer<CreateAccountState, CreateAccountEvent, CreateAccountEffect>,
+    sendEffect: (effect: CreateAccountEffect) -> Unit
+) {
     LaunchedEffect(Unit) {
         events.collect { event ->
             reducer.reduce(previousState = state.value, event = event).let {
@@ -25,6 +37,4 @@ internal fun createAccountPresenter(
             }
         }
     }
-
-    return state.value
 }
