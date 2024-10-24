c36a4d28bb8a5827b74c394b2252c5718b0e1e97
bruno13palhano <bruno13palhano@gmail.com>
2024-10-24 20:43:28 -0300

    Add destinations

--- app/src/main/java/com/bruno13palhano/loci/ui/navigation/MainNavGraph.kt
+++ app/src/main/java/com/bruno13palhano/loci/ui/navigation/MainNavGraph.kt
@@ -4,6 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.bruno13palhano.account.navigation.CreateAccountRoutes
+import com.bruno13palhano.account.navigation.createAccountScreen
+import com.bruno13palhano.forgotpassword.navigation.ForgotPasswordRoutes
+import com.bruno13palhano.forgotpassword.navigation.forgotPasswordScreen
 import com.bruno13palhano.login.navigation.LoginRoutes
 import com.bruno13palhano.login.navigation.loginScreen
 
@@ -13,10 +17,30 @@ fun MainNavGraph(
     navController: NavHostController,
     showBottomMenu: (show: Boolean) -> Unit
 ) {
-    NavHost(navController = navController, startDestination = LoginRoutes.MainLogin) {
+    NavHost(navController = navController, startDestination = LoginRoutes.Login) {
         loginScreen(
             modifier = modifier,
-            navController = navController,
+            navigateToHome = {},
+            navigateToNewAccount = {
+                navController.navigate(route = CreateAccountRoutes.CreateAccount)
+            },
+            navigateToForgotPassword = {
+                navController.navigate(route = ForgotPasswordRoutes.ForgotPassword)
+            },
+            showBottomMenu = showBottomMenu
+        )
+
+        createAccountScreen(
+            modifier = modifier,
+            navigateToHome = {},
+            navigateBack = { navController.navigateUp() },
+            showBottomMenu = showBottomMenu
+        )
+
+        forgotPasswordScreen(
+            modifier = modifier,
+            navigateToHome = {},
+            navigateBack = { navController.navigateUp() },
             showBottomMenu = showBottomMenu
         )
     }
