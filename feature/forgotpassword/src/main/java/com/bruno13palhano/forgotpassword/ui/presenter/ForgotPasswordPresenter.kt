ae43a8674bb2e049ed19bb87d09a69fcf7ddc160
bruno13palhano <bruno13palhano@gmail.com>
2024-10-24 20:37:57 -0300

    Refactor ForgotPasswordPresenter

--- feature/forgotpassword/src/main/java/com/bruno13palhano/forgotpassword/ui/presenter/ForgotPasswordPresenter.kt
+++ feature/forgotpassword/src/main/java/com/bruno13palhano/forgotpassword/ui/presenter/ForgotPasswordPresenter.kt
@@ -2,6 +2,7 @@ package com.bruno13palhano.forgotpassword.ui.presenter
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import com.bruno13palhano.ui.shared.Reducer
@@ -16,14 +17,24 @@ internal fun forgotPasswordPresenter(
 ): ForgotPasswordState {
     val state = remember { mutableStateOf(ForgotPasswordState.Initial) }
 
+    HandleEvents(events = events, state = state, reducer = reducer, sendEffect = sendEffect)
+
+    return state.value
+}
+
+@Composable
+private fun HandleEvents(
+    events: Flow<ForgotPasswordEvent>,
+    state: MutableState<ForgotPasswordState>,
+    reducer: Reducer<ForgotPasswordState, ForgotPasswordEvent, ForgotPasswordEffect>,
+    sendEffect: (effect: ForgotPasswordEffect) -> Unit
+) {
     LaunchedEffect(Unit) {
         events.collect { event ->
-            reducer.reduce(state.value, event).let {
+            reducer.reduce(previousState = state.value, event = event).let {
                 state.value = it.first
                 it.second?.let(sendEffect)
             }
         }
     }
-
-    return state.value
 }
