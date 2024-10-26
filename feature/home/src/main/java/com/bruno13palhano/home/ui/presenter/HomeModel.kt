fb2cfe492e17cafd1b4ce2ec3dc294af98e25fe4
bruno13palhano <bruno13palhano@gmail.com>
2024-10-26 12:28:39 -0300

    Add HomeModel

---
+++ feature/home/src/main/java/com/bruno13palhano/home/ui/presenter/HomeModel.kt
@@ -0,0 +1,33 @@
+package com.bruno13palhano.home.ui.presenter
+
+import androidx.compose.runtime.Immutable
+import com.bruno13palhano.ui.shared.ViewAction
+import com.bruno13palhano.ui.shared.ViewEffect
+import com.bruno13palhano.ui.shared.ViewEvent
+import com.bruno13palhano.ui.shared.ViewState
+
+@Immutable
+internal data class HomeState(
+    val loading: Boolean
+) : ViewState {
+    companion object {
+        val Initial = HomeState(
+            loading = false
+        )
+    }
+}
+
+@Immutable
+internal sealed interface HomeEvent : ViewEvent {
+    data object Refresh : HomeEvent
+}
+
+@Immutable
+internal sealed interface HomeEffect : ViewEffect {
+    data object ShowErrorInfo : HomeEffect
+}
+
+@Immutable
+internal sealed interface HomeAction : ViewAction {
+    data object Refresh : HomeAction
+}
