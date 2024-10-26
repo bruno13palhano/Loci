29fbcce4e32fc9b68c5cf0ddadf0711fe74cce22
bruno13palhano <bruno13palhano@gmail.com>
2024-10-26 12:30:57 -0300

    Add HomeViewModel

---
+++ feature/home/src/main/java/com/bruno13palhano/home/ui/viewmodel/HomeViewModel.kt
@@ -0,0 +1,32 @@
+package com.bruno13palhano.home.ui.viewmodel
+
+import androidx.compose.runtime.Composable
+import com.bruno13palhano.home.ui.presenter.HomeAction
+import com.bruno13palhano.home.ui.presenter.HomeActionProcessor
+import com.bruno13palhano.home.ui.presenter.HomeEffect
+import com.bruno13palhano.home.ui.presenter.HomeEvent
+import com.bruno13palhano.home.ui.presenter.HomeReducer
+import com.bruno13palhano.home.ui.presenter.HomeState
+import com.bruno13palhano.home.ui.presenter.homePresenter
+import com.bruno13palhano.ui.shared.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@HiltViewModel
+internal class HomeViewModel @Inject constructor(
+
+) : BaseViewModel<HomeState, HomeEvent, HomeEffect, HomeAction>(
+    actionProcessor = HomeActionProcessor(),
+    reducer = HomeReducer()
+) {
+    @Composable
+    override fun states(events: Flow<HomeEvent>): HomeState {
+        return homePresenter(
+            reducer = reducer,
+            events = events,
+            sendEvent = ::sendEvent,
+            sendEffect = ::sendEffect
+        )
+    }
+}
