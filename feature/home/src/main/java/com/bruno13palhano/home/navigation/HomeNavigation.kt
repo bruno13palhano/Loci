b48b4bea73e29dd62ac439e406e38f74f5569403
bruno13palhano <bruno13palhano@gmail.com>
2024-10-26 12:31:41 -0300

    Add HomeNavigation

---
+++ feature/home/src/main/java/com/bruno13palhano/home/navigation/HomeNavigation.kt
@@ -0,0 +1,22 @@
+package com.bruno13palhano.home.navigation
+
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.bruno13palhano.home.ui.presenter.HomeRoute
+import kotlinx.serialization.Serializable
+
+fun NavGraphBuilder.homeScreen(
+    modifier: Modifier = Modifier,
+    showBottomMenu: (show: Boolean) -> Unit
+) {
+    composable<HomeRoutes.Home> {
+        showBottomMenu(true)
+        HomeRoute(modifier = modifier)
+    }
+}
+
+sealed interface HomeRoutes {
+    @Serializable
+    data object Home : HomeRoutes
+}
