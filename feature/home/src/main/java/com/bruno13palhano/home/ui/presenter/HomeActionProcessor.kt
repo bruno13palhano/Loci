3a62dec74e9e8d6a1b22c4ab344e96e112ddc870
bruno13palhano <bruno13palhano@gmail.com>
2024-10-26 12:29:01 -0300

    Add HomeActioProcessor

---
+++ feature/home/src/main/java/com/bruno13palhano/home/ui/presenter/HomeActionProcessor.kt
@@ -0,0 +1,11 @@
+package com.bruno13palhano.home.ui.presenter
+
+import com.bruno13palhano.ui.shared.ActionProcessor
+
+internal class HomeActionProcessor : ActionProcessor<HomeAction, HomeEvent> {
+    override fun process(action: HomeAction): HomeEvent {
+        return when (action) {
+            HomeAction.Refresh -> HomeEvent.Refresh
+        }
+    }
+}
