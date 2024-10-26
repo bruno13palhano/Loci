66c9231132cca57233c26ec0f6986b642df9989e
bruno13palhano <bruno13palhano@gmail.com>
2024-10-26 17:27:08 -0300

    Add WorkspaceModel

---
+++ feature/workspace/src/main/java/com/bruno13palhano/workspace/ui/presenter/WorkspaceModel.kt
@@ -0,0 +1,31 @@
+package com.bruno13palhano.workspace.ui.presenter
+
+import androidx.compose.runtime.Immutable
+import com.bruno13palhano.ui.shared.ViewAction
+import com.bruno13palhano.ui.shared.ViewEffect
+import com.bruno13palhano.ui.shared.ViewEvent
+import com.bruno13palhano.ui.shared.ViewState
+
+@Immutable
+internal data class WorkspaceState(
+    val loading: Boolean = true
+) : ViewState {
+    companion object {
+        val Initial = WorkspaceState()
+    }
+}
+
+@Immutable
+internal sealed interface WorkspaceEvent : ViewEvent{
+    data object Refresh : WorkspaceEvent
+}
+
+@Immutable
+internal sealed interface WorkspaceEffect : ViewEffect {
+    data object ShowErrorInfo : WorkspaceEffect
+}
+
+@Immutable
+internal sealed interface WorkspaceAction : ViewAction {
+    data object OnRefresh : WorkspaceAction
+}
