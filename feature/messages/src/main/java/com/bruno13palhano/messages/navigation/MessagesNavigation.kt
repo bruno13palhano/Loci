45df672bf7870895f24ca6cc5a8694183107eb51
bruno13palhano <bruno13palhano@gmail.com>
2024-10-27 22:10:23 -0300

    Add MessagesNavigation

--- feature/messages/src/main/java/com/bruno13palhano/messages/navigation/MessagesNavigation.kt
+++ feature/messages/src/main/java/com/bruno13palhano/messages/navigation/MessagesNavigation.kt
@@ -1,4 +1,22 @@
 package com.bruno13palhano.messages.navigation
 
-class MessagesNavigation {
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.bruno13palhano.messages.ui.presenter.MessagesRoute
+import kotlinx.serialization.Serializable
+
+fun NavGraphBuilder.messagesNavigation(
+    modifier: Modifier = Modifier,
+    showBottomMenu: (show: Boolean) -> Unit
+) {
+    composable<MessagesRoutes.Messages> {
+        showBottomMenu(true)
+        MessagesRoute(modifier = modifier)
+    }
+}
+
+sealed interface MessagesRoutes {
+    @Serializable
+    data object Messages : MessagesRoutes
 }
