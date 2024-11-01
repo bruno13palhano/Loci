6a4f02e02f397555fa178d0d55ddf12b8c1674c7
bruno13palhano <bruno13palhano@gmail.com>
2024-11-01 07:35:54 -0300

    Add RemoteFreelancer methods

--- core/network/src/main/java/com/bruno13palhano/network/api/freelancer/RemoteFreelancer.kt
+++ core/network/src/main/java/com/bruno13palhano/network/api/freelancer/RemoteFreelancer.kt
@@ -1,3 +1,20 @@
 package com.bruno13palhano.network.api.freelancer
 
-interface RemoteFreelancer
+import com.bruno13palhano.model.Contract
+import com.bruno13palhano.model.Service
+
+interface RemoteFreelancer {
+    suspend fun insertService(service: Service)
+
+    suspend fun updateService(service: Service)
+
+    suspend fun deleteService(serviceId: Long)
+
+    suspend fun getServices(): List<Service>
+
+    suspend fun insertContract(contract: Contract)
+
+    suspend fun cancelContract(contractId: Long)
+
+    suspend fun getContracts(): List<Contract>
+}
--- core/network/src/main/java/com/bruno13palhano/network/api/freelancer/RemoteFreelancerDataSource.kt
+++ core/network/src/main/java/com/bruno13palhano/network/api/freelancer/RemoteFreelancerDataSource.kt
@@ -1,4 +1,34 @@
 package com.bruno13palhano.network.api.freelancer
 
+import com.bruno13palhano.model.Contract
+import com.bruno13palhano.model.Service
+
 internal class RemoteFreelancerDataSource : RemoteFreelancer {
+    override suspend fun insertService(service: Service) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateService(service: Service) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteService(serviceId: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getServices(): List<Service> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertContract(contract: Contract) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun cancelContract(contractId: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getContracts(): List<Contract> {
+        TODO("Not yet implemented")
+    }
 }
