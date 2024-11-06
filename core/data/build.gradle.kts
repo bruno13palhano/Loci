85189dd7ff4146f04ac7d4391be23d619db46a93
bruno13palhano <bruno13palhano@gmail.com>
2024-11-05 22:28:38 -0300

    Add network dependency

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(project(":core:model"))
+    implementation(project(":core:network"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
