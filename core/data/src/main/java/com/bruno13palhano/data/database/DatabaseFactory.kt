d2fcd55fe28074e8dd6e48e1e3f541133d941f81
bruno13palhano <bruno13palhano@gmail.com>
2024-11-08 20:17:14 -0300

    Add database and sql driver factories

---
+++ core/data/src/main/java/com/bruno13palhano/data/database/DatabaseFactory.kt
@@ -0,0 +1,9 @@
+package com.bruno13palhano.data.database
+
+import com.bruno13palhano.cache.AppDatabase
+
+internal class DatabaseFactory(private val driverFactory: DriverFactory) {
+    fun createDatabase(): AppDatabase {
+        return AppDatabase(driver = driverFactory.createDriver())
+    }
+}
